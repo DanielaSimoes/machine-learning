babca455a0ca06cd9e8884dc1c96eb8298a5bae9
Daniela Simões <danielasimoes@ua.pt>
2018-10-30 09:19:11 +0000

    verifying linear regression results.

--- regression_modeling.py
+++ regression_modeling.py
@@ -1,14 +1,14 @@
 import pandas as pd
 from sklearn.linear_model import LinearRegression
 from sklearn.cross_validation import train_test_split
-from sklearn.metrics import mean_squared_error
+from sklearn.metrics import mean_squared_error, r2_score
 
 import matplotlib.pyplot as plt
 import seaborn
 import statsmodels.formula.api as smf
 
 
-# # ______________________________ Linear Regression_____________________________________ #
+# # ______________________________ Linear Regression _____________________________________ #
 def linear(wine_set):
 
     X = wine_set[['quality']]
@@ -16,14 +16,15 @@ def linear(wine_set):
     y = wine_set[['fixed acidity', 'volatile acidity', 'citric acid', 'residual sugar', 'chlorides', 'free sulfur dioxide',
             'total sulfur dioxide', 'density', 'pH', 'sulphates', 'alcohol']]
 
+    # 30% of the dataset is used for testing and 70% for training
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
 
     model = LinearRegression()
     model = model.fit(y_train, X_train)
 
     predicted_data = model.predict(y_test)
-    print(mean_squared_error(X_test, predicted_data))
-
+    print("Mean squared error:", mean_squared_error(X_test, predicted_data))
+    print("Coefficient of determination:", r2_score(X_test, predicted_data))
 
 if __name__ == '__main__':
     white = pd.read_csv('winequality-white.csv', low_memory=False, sep=';')
