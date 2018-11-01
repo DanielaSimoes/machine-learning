0dc2ab0fa2ba3e4c7af93afe7ee1f1df36103345
r-Guedes <44370690+r-Guedes@users.noreply.github.com>
2018-11-01 17:55:57 +0000

    Add files via upload
    Logisctic Regression

---
+++ logistic_regression.py
@@ -0,0 +1,32 @@
+import pandas as pd
+from sklearn.linear_model import LogisticRegression
+from sklearn.cross_validation import train_test_split
+from sklearn.metrics import mean_squared_error, r2_score
+
+import matplotlib.pyplot as plt
+import seaborn
+import statsmodels.formula.api as smf
+
+
+# # ______________________________ Linear Regression _____________________________________ #
+def linear(wine_set):
+
+    X = wine_set[['quality']]
+
+    y = wine_set[['fixed acidity', 'volatile acidity', 'citric acid', 'residual sugar', 'chlorides', 'free sulfur dioxide',
+            'total sulfur dioxide', 'density', 'pH', 'sulphates', 'alcohol']]
+	
+	
+    # 30% of the dataset is used for testing and 70% for training
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
+
+    model = LogisticRegression()
+    model = model.fit(y_train, X_train)
+
+    predicted_data = model.predict(y_test)
+    print("Mean squared error:", mean_squared_error(X_test, predicted_data))
+    print("Coefficient of determination:", r2_score(X_test, predicted_data)*100,"%")
+
+if __name__ == '__main__':
+    white = pd.read_csv('winequality-red.csv', low_memory=False, sep=';')
+    linear(white)
