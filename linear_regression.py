49db12c6810455e3e038c1803e67c0b1d4c021fa
Daniela Simões <danielasimoes@ua.pt>
2018-11-03 22:45:15 +0000

    linear regression data normalization.

--- linear_regression.py
+++ linear_regression.py
@@ -12,15 +12,19 @@ def linear(wine_set):
     y = wine_set[['fixed acidity', 'volatile acidity', 'citric acid', 'residual sugar', 'chlorides', 'free sulfur dioxide',
             'total sulfur dioxide', 'density', 'pH', 'sulphates', 'alcohol']]
 
-    # 30% of the dataset is used for testing and 70% for training
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
+    # 20% of the dataset is used for testing and 80% for training
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, train_size=0.8)
 
-    model = LinearRegression()
+    model = LinearRegression(normalize=True)
     model = model.fit(y_train, X_train)
 
     predicted_data = model.predict(y_test)
+    params = model.get_params()
+
+    print("Params: ", params)
     print("Mean squared error:", mean_squared_error(X_test, predicted_data))
     print("Coefficient of determination:", r2_score(X_test, predicted_data)*100,"%")
+
 
 if __name__ == '__main__':
     white = pd.read_csv('winequality-white.csv', low_memory=False, sep=';')
