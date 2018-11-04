16814dc4d47cead51eb6c065058bd2e960dff99f
Daniela Simões <danielasimoes@ua.pt>
2018-11-04 20:12:58 +0000

    accuracy of 75% in NN

--- neuronal_networks/train_wine_model.py
+++ neuronal_networks/train_wine_model.py
@@ -2,37 +2,47 @@ from keras.models import Sequential
 from keras.layers import Dense
 import keras
 from sklearn.cross_validation import train_test_split
-import pandas as pd
+import numpy
+from keras import optimizers
+
 
 
 if __name__ == '__main__':
-    dataset = pd.read_csv('../winequality-white.csv', low_memory=False, sep=';')
+    dataset = numpy.loadtxt("../winequality-white.csv", delimiter=";", skiprows=1)
 
-    # number of classifications
-    classifications = 10
+    # number of classifications - bad, medium and good
 
-    y = dataset[['quality']]
+    y = dataset[:, 11]
+    classifications = 3
 
-    X = dataset[
-        ['fixed acidity', 'volatile acidity', 'citric acid', 'residual sugar', 'chlorides', 'free sulfur dioxide',
-         'total sulfur dioxide', 'density', 'pH', 'sulphates', 'alcohol']]
+    new_y = []
 
-    # 30% of the dataset is used for testing and 70% for training
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, train_size=0.8)
+    for each in y:
+        if 0 <= each <= 4:
+            # 0, 1, 2, 3, 4
+            new_y.append(0)
+        elif 5 <= each <= 6:
+            # 5, 6
+            new_y.append(1)
+        else:
+            # 7, 8, 9, 10
+            new_y.append(2)
 
-    y_train = keras.utils.to_categorical(y_train-1, classifications)
-    y_test = keras.utils.to_categorical(y_test- 1, classifications)
+    y = new_y
+
+    X = dataset[:, 0:11]
+
+    x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=5)
 
     model = Sequential()
-    model.add(Dense(64, input_dim=11, activation='relu'))
-    model.add(Dense(50, activation='relu'))
-    model.add(Dense(40, activation='relu'))
-    model.add(Dense(30, activation='relu'))
-    model.add(Dense(20, activation='relu'))
+    model.add(Dense(10, input_dim=11, activation='relu'))
+    model.add(Dense(8, activation='relu'))
+    model.add(Dense(6, activation='relu'))
     model.add(Dense(classifications, activation='softmax'))
 
-    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+    adam = optimizers.adam(lr=0.01, clipnorm=1.)
+    model.compile(loss='sparse_categorical_crossentropy', optimizer=adam, metrics=['accuracy'])
 
-    model.fit(X_train, y_train, epochs=200,validation_data=(X_test, y_test), batch_size=50, verbose=2)
+    model.fit(x_train, y_train, epochs=5000, batch_size=50, verbose=2, validation_data=(x_test, y_test))
 
     model.save('wine-model.h5')
