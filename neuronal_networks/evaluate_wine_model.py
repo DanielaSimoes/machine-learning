df7d38368ef879d742a07afe52e4d8903ceeb846
Daniela Simões <danielasimoes@ua.pt>
2018-11-15 11:33:22 +0000

    evaluate ok.

--- neuronal_networks/evaluate_wine_model.py
+++ neuronal_networks/evaluate_wine_model.py
@@ -1,9 +1,5 @@
-from sklearn.metrics import mean_squared_error, r2_score, accuracy_score
-from keras import losses
-import numpy as np
 
 
 def evaluate(model, x_train, y_train):
-    y_train = np.array(y_train)
     score = model.evaluate(x_train, y_train, batch_size=32)
     print(score)
