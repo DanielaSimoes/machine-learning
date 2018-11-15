60a292801d95a915d16d871f8d774ab0ccf56043
ruiguedes <r.guedes@ua.pt>
2018-11-15 11:15:33 +0000

    update dataset division

--- neuronal_networks/neuronal_network.py
+++ neuronal_networks/neuronal_network.py
@@ -1,5 +1,5 @@
 import numpy
-from sklearn.cross_validation import train_test_split
+from sklearn.model_selection import train_test_split
 from optimize import optimize
 from train_wine_model import nn_train
 from evaluate_wine_model import evaluate
@@ -33,8 +33,9 @@ if __name__ == '__main__':
 
     x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=5)
 
+    x_train_60, x_val, y_train_60, y_val = train_test_split(x_train, y_train, test_size=0.25, random_state=5)
     # best_parameters
-    best_parameters = optimize(x_train, y_train, x_test, y_test)
+    best_parameters = optimize(x_train_60, y_train_60, x_val, y_val)
 
     # Pass the best parameters to train, and the Train Data
     trained_model = nn_train(x_train, y_train, x_test, y_test, best_parameters)
@@ -47,4 +48,4 @@ if __name__ == '__main__':
     evaluate(trained_model, x_train, y_train)
 
     # Test the model
-    #test(trained_model, x_test, y_test)
+    test(trained_model, x_test, y_test)
--- neuronal_networks/test_model.py
+++ neuronal_networks/test_model.py
@@ -5,11 +5,11 @@ from sklearn.model_selection import cross_val_score
 def test(model, x_test, y_test):
     score = model.evaluate(x_test, y_test, batch_size=32)
     print(score)
-    predicted_data = model.predict(x_test)
+    #predicted_data = model.predict(x_test)
 
-    print("Score CV - test set:", cross_val_score(model, x_test, y_test, cv=10).mean())
-    print("Mean squared error - training set:", mean_squared_error(y_test, predicted_data))
-    print("Coefficient of determination - training set:", r2_score(y_test, predicted_data))
-    print("Accuracy - training set:", accuracy_score(y_test, predicted_data))
+    #print("Score CV - test set:", cross_val_score(model, x_test, y_test, cv=10).mean())
+    #print("Mean squared error - training set:", mean_squared_error(y_test, predicted_data))
+    #print("Coefficient of determination - training set:", r2_score(y_test, predicted_data))
+    #print("Accuracy - training set:", accuracy_score(y_test, predicted_data))
 
 
